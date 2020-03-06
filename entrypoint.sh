4f2cdaaf614efc0607284a035dd4158daf6d88e4
jlo <13147870+jesseloudon@users.noreply.github.com>
2020-03-07 01:33:32 +1100

    include az login with sp

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+if [ -z "$INPUT_SP_CLIENT_ID" ]; then
+  echo "Service Principal Client ID is not specified - Exiting"
+  exit 1
+fi
+
+if [ -z "$IINPUT_SP_CLIENT_SECRET" ]; then
+  echo "Service Principal CLient Secret is not specified - Exiting"
+  exit 1
+fi
+
+if [ -z "$INPUT_TENANT_ID" ]; then
+  echo "Tenant ID is not specified - Exiting"
+  exit 1
+fi
+
 if [ -z "$INPUT_RESOURCEGROUP_NAME" ]; then
   echo "Resource Group name is not specified - Exiting"
   exit 1
@@ -16,6 +31,9 @@ if [ -z "$INPUT_CONFIGURE_FIREWALL_DEFAULT_ACTION" ]; then
   echo "Default Action (Allow or Deny) for Storage Account Firewall is not specified - Exiting"
   exit 1
 fi
+
+#Azure AuthN with Service Principal
+az login --service-principal -u ${INPUT_SP_CLIENT_ID} -p ${INPUT_SP_CLIENT_SECRET} --tenant ${INPUT_TENANT_ID}
 
 # Configure Azure Storage Firewall Default Action
 az storage account update --resource-group ${INPUT_RESOURCEGROUP_NAME} --name ${INPUT_STORAGEACCOUNT_NAME} --default-action ${INPUT_CONFIGURE_FIREWALL_DEFAULT_ACTION}
